6b706f44d73d6ab9a62429d823fb8fff22bbb5f1
lishmimd <51526771+lishmimd@users.noreply.github.com>
2019-07-30 15:17:26 +0530

    Add files via upload

---
+++ ICD10Controller.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homecare.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ICD10Controller : ControllerBase
+    {
+        // GET: api/ICD10
+        [HttpGet]
+        public IEnumerable<icd10Class> Get()
+        {
+            List<icd10Class> icd10List = new List<icd10Class>();
+
+            icd10Class a1 = new icd10Class("A102", "abc");
+            icd10List.Add(a1);
+
+            return icd10List;
+
+
+        }
+
+        // GET: api/ICD10/5
+        [HttpGet("{id}", Name = "icd10Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/ICD10
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/ICD10/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
+public class icd10Class
+{
+    public icd10Class(string code1, string display1)
+    {
+        code =code1;
+        display= display1;
+        
+    }
+    public string code { get; set; }
+    public string display { get; set; }
+
+    internal static void Add(icd10Class a1)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+    
+
+
---
+++ PayerController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homecare.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PayerController : ControllerBase
+    {
+        // GET: api/Payer
+        [HttpGet]
+        public IEnumerable<Payer> Get()
+        {
+            List<Payer> payerlist= new List<Payer>();
+
+            Payer c1 = new Payer("Lishmi", "Davis", "2345678987", "kdlr");
+            payerlist.Add(c1);
+
+            return payerlist;
+        }
+
+        // GET: api/Payer/5
+        [HttpGet("{id}", Name = "PayerGet")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Payer
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Payer/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
+public class Payer
+{
+    public Payer(string lname, string fname, string contact, string discipline1)
+    {
+        lastName = lname;
+       firstName = fname;
+        contactNumber = contact;
+        city = discipline1;
+    }
+    public string lastName { get; set; }
+    public string firstName { get; set; }
+    public string contactNumber { get; set; }
+    public string city { get; set; }
+}
